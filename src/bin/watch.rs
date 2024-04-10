ffe95f674d9dabc2eeaf7ce48704d936fb4efab0
Christophe Rosset <tophe@topheman.com>
2024-04-10 19:42:51 +0200

    feat: setup bin watch/play

---
+++ src/bin/watch.rs
@@ -0,0 +1,4 @@
+fn main() -> anyhow::Result<()> {
+  println!("watch");
+  Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    println!("This isn't the entry point. Look for:");
+    println!("cargo run --bin play");
+    println!("cargo run --bin watch");
 }
