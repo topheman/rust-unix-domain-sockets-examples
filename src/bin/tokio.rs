b75abd7a73aae9fd5c96c610898b95999460e2b3
Christophe Rosset <tophe@topheman.com>
2024-05-05 11:46:58 +0200

    feat: basic implementation of play/watch

---
+++ src/bin/tokio.rs
@@ -0,0 +1,52 @@
+use anyhow::Context;
+use std::io;
+use tokio::io::Interest;
+use tokio::net::UnixStream;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // let dir = tempfile::tempdir().unwrap();
+    // let bind_path = dir.path().join("bind_path");
+
+    let socket_path = "mysocket";
+    let stream = UnixStream::connect(socket_path).await?;
+
+    loop {
+        let ready = stream
+            .ready(Interest::READABLE | Interest::WRITABLE)
+            .await?;
+
+        if ready.is_readable() {
+            let mut data = vec![0; 1024];
+            // Try to read data, this may still fail with `WouldBlock`
+            // if the readiness event is a false positive.
+            match stream.try_read(&mut data) {
+                Ok(n) => {
+                    println!("read {} bytes", n);
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                    continue;
+                }
+                Err(e) => {
+                    return Err(e.into());
+                }
+            }
+        }
+
+        if ready.is_writable() {
+            // Try to write data, this may still fail with `WouldBlock`
+            // if the readiness event is a false positive.
+            match stream.try_write(b"hello world") {
+                Ok(n) => {
+                    println!("write {} bytes", n);
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                    continue;
+                }
+                Err(e) => {
+                    return Err(e.into());
+                }
+            }
+        }
+    }
+}
